ac28712c69ffbd473a532c7b722eb7d02bce4967
SuperToad <super-toad@hotmail.fr>
2018-02-02 11:53:33 +0100

    Add files via upload

---
+++ csp.cpp
@@ -0,0 +1,25 @@
+
+enum nature {DIFFERENCE,NAIRES};
+
+typedef struct
+{
+	int valeur;
+	
+} Variable;
+
+typedef struct
+{
+	int [] portee;
+	int arite;
+	nature nat; // 0 : binaire de difference 1 : n-aires
+	int valeur; // Si nat == DIFFERENCE
+	
+} Contrainte;
+
+
+typedef struct
+{
+	
+	
+	
+} Csp;
